c50fca602d56a2d65de1ad7cb28b3e7e44d6f089
jhhoward <jhhoward@ncsu.edu>
2022-11-01 15:11:03 -0400

    imports and empty main for ddp

---
+++ ddp.py
@@ -0,0 +1,15 @@
+#  General Imports
+from collections import namedtuple
+from abc import ABC, abstractmethod
+import numpy as np
+from scipy.linalg import eigh
+from scipy.linalg import null_space
+from matplotlib import pyplot as plt
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
+
+
